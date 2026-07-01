6815542460462f648f138c7204ca2840d5cbfece
Valentin Patti <gonzalezvalentin62c1@gmail.com>
2026-07-01 14:26:53 -0300

    agregue el crud de proveedores

--- Backend/routes/index.routes.js
+++ Backend/routes/index.routes.js
@@ -2,16 +2,16 @@ const express = require("express")
 const indexRouter = express.Router();
 const rutaInicioSesion = require('./ingreso.routes.js')
 const rutaEmpleado = require("./empleados.routes.js");
+const rutaProveedor = require("./proveedor.routes.js");
 //const rutaMedicamento = require('./medicamento.routes.js')
-//const rutaProveedor = require("./proveedor.routes.js");
 //const rutaCliente = require("./clientes.routes.js");
 // const rutaVenta = require("./venta.routes.js");
 // const rutaCompra = require("./compras.routes.js");
 
 indexRouter.use("/ingreso", rutaInicioSesion);
 indexRouter.use("/empleados", rutaEmpleado);
+indexRouter.use("/proveedores", rutaProveedor);
 // indexRouter.use('/medicamentos', rutaMedicamento)
-// indexRouter.use("/proveedores", rutaProveedor);
 // indexRouter.use("/clientes", rutaCliente);
 // indexRouter.use("/ventas", rutaVenta);
 // indexRouter.use("/compras", rutaCompra);
