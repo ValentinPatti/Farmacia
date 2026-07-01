90b0c8c0089bfe49b9a342ac67839adb234afddd
Valentin Patti <gonzalezvalentin62c1@gmail.com>
2026-06-30 23:08:54 -0300

    agregue la conexion a la db, variables de entorno, middlewares

---
+++ Backend/middlewares/rol.middleware.js
@@ -0,0 +1,13 @@
+const verificarRol = (...rolesPermitidos) => {
+
+    return (req, res, next) => {
+        if (!rolesPermitidos.includes(req.user.rol.toLowerCase())) {
+            return res.status(403).json({
+                message: "No tiene permisos para acceder"
+            });
+        }
+        next();
+    };
+};
+
+module.exports = verificarRol;
